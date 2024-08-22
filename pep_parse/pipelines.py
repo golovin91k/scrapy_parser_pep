de77d685507686a3e445c87d83531341fb3c85ee
Kirill Golovin <golovin91k@gmail.com>
2024-08-22 21:55:23 +0300

    1try

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,37 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from collections import defaultdict
+import csv
+import datetime as dt
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = 'results/'
+
+
+list_of_statuses = []
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        list_of_statuses.append(item['status'])
         return item
+
+    def close_spider(self, spider):
+        results_dict = defaultdict(int)
+        for status in list_of_statuses:
+            results_dict[status] += 1
+        results = []
+        for key, value in results_dict.items():
+            results.append((key, value))
+        results.append(('Total', len(list_of_statuses)))
+
+        now = dt.datetime.now()
+        now_formatted = now.strftime('%Y-%m-%d_%H-%M-%S')
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_path = BASE_DIR / file_name
+        with open(file_path, 'w', encoding='utf-8') as f:
+            file_writer = csv.writer(f, delimiter=',', lineterminator='\r')
+            file_writer.writerow(['Status', 'Quantity'])
+            for row in results:
+                file_writer.writerow(row)
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,27 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/', ]
 
     def parse(self, response):
-        pass
+        numerical_index_peps_table = response.css(
+            'section[id="numerical-index"]').css('a[href^="pep"]')
+        for pep_link in numerical_index_peps_table[0:]:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number = ' '.join((response.css(
+            'h1[class="page-title"]::text').get()).split()[:2])
+        name = ' '.join((response.css(
+            'h1[class="page-title"]::text').get()).split()[3:])
+        data = {
+            'status': response.css('abbr::text').get(),
+            'name': name,
+            'number': number,
+        }
+        yield PepParseItem(data)
